0bb3d559a01219ddb8abd6cae5f43a02c2f74f57
Cooper Senior <116678356+coopersenior@users.noreply.github.com>
2023-02-28 15:23:13 -0700

    added range func

--- normalizeRange.py
+++ normalizeRange.py
@@ -2,4 +2,5 @@ import numpy as np
 
 #A function that normalizes the attributes in a two-dimensional numpy array using range normalization. 
 def normalizeRange(arr:np.ndarray):
-    pass
+    normalized = (arr - np.min(arr)) / (np.max(arr) - np.min(arr))
+    return normalized
