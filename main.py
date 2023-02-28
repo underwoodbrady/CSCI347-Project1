8ca617e771b44fc5b9ec8cc26eafef6ee702b54f
Brady Underwood <underwood.brady@gmail.com>
2023-02-28 14:38:07 -0700

    handleData

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from handleData import handleData
 from multidimensionalMean import multidimensionalMean
 from sampleCovariance import sampleCovariance
 from attributeCorrelation import attributeCorrelation
@@ -8,14 +9,9 @@ from normalizeStandard import normalizeStandard
 from covarianceMatrix import covarianceMatrix
 from labelEncode import labelEncode 
 
-IN_FILE = './machine.data'
-
-#Convert data from 'machine.data' into np arrays
-def handleData():
-    pass
-
 #Call each of the above functions with the dataset
 def main():
+    handleData();
     multidimensionalMean()
     sampleCovariance()
     attributeCorrelation()
