1ce2d97144b3c9671a365cd8e4890e501d08676c
Brady Underwood <underwood.brady@gmail.com>
2023-02-28 13:41:09 -0700

    minor cleanup

--- attributeCorrelation.py
+++ attributeCorrelation.py
@@ -2,6 +2,4 @@ import numpy as np
 
 #A function that computes the correlation between two attributes that are input as two numpy vectors. 
 def attributeCorrelation(vec1:np.array, vec2:np.array):
-    print("hello")
-
-attributeCorrelation()
+    pass
