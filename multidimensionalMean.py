f0b26154f05c37e22c562d837fe5d9fd685bb9ab
Cooper Senior <116678356+coopersenior@users.noreply.github.com>
2023-02-28 15:38:20 -0700

    added in functions

--- multidimensionalMean.py
+++ multidimensionalMean.py
@@ -2,4 +2,9 @@ import numpy as np
 
 #A function that computes the mean of a numerical, multidimensional data set input as a 2-dimensional numpy array
 def multidimensionalMean(arr:np.ndarray):
-    pass
+    count = 0
+    total = 0
+    for index, value in np.ndenumerate(arr):
+        count += 1
+        total += value
+    return total / count
--- normalizeStandard.py
+++ normalizeStandard.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+from multidimensionalMean import multidimensionalMean
+
 #A function that normalizes the attributes in a two-dimensional numpy array using standard normalization. 
 def normalizeStandard(inp:np.ndarray):
-    pass
+    mn = multidimensionalMean(inp)  # already written mean function
+    std = np.std(inp)  # might have to write the standard deviation function ourselves
+    return (inp - mn) / std
