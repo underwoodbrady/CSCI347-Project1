e73bcd4a8bfdf815033fd1e380da2ba76ac4ba03
Cooper Senior <116678356+coopersenior@users.noreply.github.com>
2023-02-28 15:09:28 -0700

    added in code

--- sampleCovariance.py
+++ sampleCovariance.py
@@ -1,5 +1,14 @@
 import numpy as np
 
+from multidimensionalMean import multidimensionalMean
+
 #A function that computes the sample covariance between two attributes that are input as one-dimensional numpy vectors 
 def sampleCovariance(vec1:np.array, vec2:np.array):
-    pass
+    n = len(vec1)
+    mean_x = multidimensionalMean(vec1)  # using previously written mean function
+    mean_y = multidimensionalMean(vec2)
+    covariance = 0
+    for i in range(n):
+        covariance += (vec1[i] - mean_x) * (vec2[i] - mean_y)
+    covariance /= n - 1
+    return covariance
