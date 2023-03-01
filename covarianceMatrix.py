46b585ca6d222deb56eb80ee9724c7a7b9d08987
Cooper Senior <116678356+coopersenior@users.noreply.github.com>
2023-02-28 18:30:22 -0700

    added in functions

--- covarianceMatrix.py
+++ covarianceMatrix.py
@@ -1,5 +1,16 @@
 import numpy as np
 
+from multidimensionalMean import multidimensionalMean
+
 #A function that computes the covariance matrix of a data set. 
 def covarianceMatrix(inp:np.ndarray):
-    pass
+    means = multidimensionalMean(inp)
+    centered = inp - means.reshape(-1, 1)
+    
+    n = centered.shape[1]
+    cov_mat = np.zeros((centered.shape[0], centered.shape[0]))
+    for i in range(centered.shape[0]):
+        for j in range(centered.shape[0]):
+            cov_mat[i][j] = np.sum(centered[i] * centered[j]) / (n - 1)
+
+    return cov_mat
