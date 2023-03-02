f55c15ded5a4ccc4f83d06f61588f017aabbb679
Brady Underwood <underwood.brady@gmail.com>
2023-03-02 15:57:48 -0700

    first iteration of label encoding

--- labelEncode.py
+++ labelEncode.py
@@ -1,28 +1,20 @@
 import numpy as np
 
-#Implement hash map functionality
-#A function that label-encodes a two-dimensional categorical data array that is passed in as input. 
-def labelEncode(inp:np.ndarray):
-    hash1 = []
-    hash2 = []
-    for i in range(len(inp)):
-        if inp[i][0] not in hash1:
-            hash1.append(inp[i][0])
-            #print(inp[i][0], "1 included")
 
-        #else:
-            #print(inp[i][0], "1 gotta add")
-            #hash1.append(inp[i][0])
+fruits = np.array([['apple', 'banana', 'grape', 'apple', 'apple', 'grape'],['carrot', 'lettuce', 'lettuce', 'lettuce', 'tomato', 'cucumber']])
 
-        if inp[i][1] not in hash2:
-            hash2.append(inp[i][1])
-            #print(inp[i][1], "2 included")
+def labelEncode(arr):
+    values = {}
+    output = []
+    inc = 1
+    for i in range(len(arr)):
+        if values.get(arr[i]) != None:
+            output.append(values.get(arr[i]))
+        else:
+            values[arr[i]] = inc
+            output.append(inc)
+            inc +=1
+    return output
 
-        #else:
-            #print(inp[i][1], "2 gotta add")
-            #hash2.append(inp[i][1])
-    return hash1, hash2
-    #print(hash1)
-    #print(hash2)
 
-#labelEncode(np.array([["categorical", "array"],["categorical", "dog"]]))
+print(labelEncode(fruits[0]), labelEncode(fruits[1]))
